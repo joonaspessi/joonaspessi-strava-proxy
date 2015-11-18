7848756eda3bd29476c2dd62b6540bed27b14a30
Joonas Pessi <joonas.pessi@futurice.com>
2015-11-18 22:06:34 +0200

    fine tuned initialization message

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ app.use(function(req, res, next) {
 app.use("/api/v1/", strava);
 
 let server = app.listen(3000, function() {
-    let host = server.address().address;
     let port = server.address().port;
-    console.log('Example app listening at http://%s:%s', host, port);
+    console.log("Strava proxy listening at port", port);
 });
